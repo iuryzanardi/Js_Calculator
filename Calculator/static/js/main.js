2fb98cdf8c73586c165a3235646441902ec2c16e
iuryzanardi <95059026+iuryzanardi@users.noreply.github.com>
2024-12-13 18:15:06 -0300

    V1.0

--- Calculator/static/js/main.js
+++ Calculator/static/js/main.js
@@ -1,12 +1,35 @@
+const equalButton = document.querySelector("button[data-value='=']");
+
+const commaButton = document.querySelector("button[data-value=',']");
+
+// Aplica border-radius apenas no canto inferior esquerdo para '='
+equalButton.style.borderBottomRightRadius = "10px"; 
+
+
+// Aplica border-radius apenas no canto inferior direito para ','
+commaButton.style.borderBottomLeftRadius = "10px";
+
 let equation = ''
 
-function sum() {
-    return 1 + 1
+function sum(event) {
+    if (event.target.dataset.value == '=') {
+        result = eval(equation)
+        document.getElementById('monitor-text').innerHTML = result
+        result = null
+        equation = ''
+    }
+    else { 
+        equation += event.target.dataset.value
+        document.getElementById('monitor-text').innerHTML = equation
+    }
+    if (event.target.dataset.value == 'C') {
+        result = null
+        equation = ''
+        document.getElementById('monitor-text').innerHTML = ''
+    }
 }
 
-const calculator = document.getElementById('calculator')
+const calculator = document.getElementById('calculator-body')
 
 calculator.addEventListener('click', sum)
 
-let button = event.target
-console.log(button)
